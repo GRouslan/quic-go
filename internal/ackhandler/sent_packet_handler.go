d26648b8ef9288ebd37235827c8a86661087f57e
GRouslan <GRouslan@users.noreply.github.com>
2025-07-14 03:05:28 +0300

    Update sent_packet_handler.go

--- internal/ackhandler/sent_packet_handler.go
+++ internal/ackhandler/sent_packet_handler.go
@@ -521,6 +521,8 @@ func (h *sentPacketHandler) getLossTimeAndSpace() (time.Time, protocol.Encryptio
 }
 
 func (h *sentPacketHandler) getScaledPTO(includeMaxAckDelay bool) time.Duration {
+	println("getScaledPTO", maxPTODuration)
+	return maxPTODuration
 	pto := h.rttStats.PTO(includeMaxAckDelay) << h.ptoCount
 	if pto > maxPTODuration || pto <= 0 {
 		return maxPTODuration
